fe7c15ce9bdb8aeaa183bb25ea5cfc00987ee35c
IanaPushcash <anny_ok-94@mail.ru>
2017-12-14 11:55:31 +0200

    asp.net dwh init

---
+++ dwh/DataWarehouse/ASP.net dwh/API/API/Controllers/CityController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataWarehouse.Models;
+
+namespace API.Controllers
+{
+	public class CityController : ApiController
+	{
+		// GET api/<controller>
+		[Queryable]
+		public IQueryable<City> Get()
+		{
+			using (var db = new DatabaseContext())
+			{
+				return db.Cities;
+			}
+		}
+
+		// GET api/<controller>/5
+		public City Get(int id)
+		{
+			using (var db = new DatabaseContext())
+			{
+				return db.Cities.First(c=>c.Id == id);
+			}
+		}
+
+		// POST api/<controller>
+		public void Post([FromBody]City value)
+		{
+			using (var db = new DatabaseContext())
+			{
+				db.Cities.Add(value);
+			}
+		}
+
+		// PUT api/<controller>/5
+		public void Put(int id, [FromBody]City value)
+		{
+			using (var db = new DatabaseContext())
+			{
+				db.Cities.Add(value);
+			}
+		}
+
+		// DELETE api/<controller>/5
+		public void Delete(int id)
+		{
+
+		}
+	}
+}
---
+++ dwh/DataWarehouse/ASP.net dwh/API/API/Models/Pagination.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataWarehouse.Models
+{
+	abstract class Pagination
+	{
+		public int PageNumber { get; set; }
+		public int RecPerPage { get; set; }
+	}
+}
